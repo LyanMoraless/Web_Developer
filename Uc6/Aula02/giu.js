a078b628d5aa90ede4f7e53995fd8f23ba5a1dfa
LyanMoraless <drlyanmorales@gmail.com>
2022-05-09 17:35:00 -0300

    Adicionando Uc6/Aula02

---
+++ Uc6/Aula02/giu.js
@@ -0,0 +1,26 @@
+const prompt = require('prompt-sync')()
+
+console.log('')
+
+let name = prompt('Digite o nome de uma pessoa: ');
+let name2 = prompt('Digite o seu nome: ');
+
+console.log('')
+
+let car2 = prompt('Digite uma caracteristica: ')
+let car3 = prompt('Digite uma caracteristica: ')
+let car4 = prompt('Digite uma caracteristica: ')
+let car5 = prompt('Digite uma caracteristica: ')
+let car6 = prompt('Digite uma caracteristica: ')
+let car7 = prompt('Digite uma caracteristica: ')
+
+
+
+console.log('')
+console.log('-------------------------------')
+console.log('')
+
+console.log('O usuário', name, 'para o', name2, 'é', car2, car3, car4, car5, car6, car7, 'e todo adjetivo que houver no universo')
+
+console.log('')
+console.log('-------------------------------')
---
+++ Uc6/Aula02/modulo.js
@@ -0,0 +1,10 @@
+let figurinhas = 75;
+let pags = 9;
+
+let divisao = figurinhas / pags
+let modulo = figurinhas % pags
+
+let falta = divisao - modulo
+
+console.log(falta.toFixed(0))
+
