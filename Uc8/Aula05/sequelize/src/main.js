cc79956fe585ce718a0b0a6822147eecdaeacdc7
LyanMoraless <drlyanmorales@gmail.com>
2022-06-20 17:25:14 -0300

    Sequelize

--- Uc8/Aula05/sequelize/src/main.js
+++ Uc8/Aula05/sequelize/src/main.js
@@ -1,5 +1,6 @@
 //RODAR: NODE MAIN.JS
 const { sequelize, Area, Aluno } = require('./models');
+const colors = require('colors');
 
 (async () => {
     await sequelize.sync({ force: true, logging: true });
@@ -31,45 +32,45 @@ const { sequelize, Area, Aluno } = require('./models');
     // ----------------------------------> <---------------------------------- //
     console.log('');
 
-    console.log("Buscando todos os alunos");
+    console.log("Buscando todos os alunos".red.underline);
 
     const alunos = await Aluno.findAll();
     console.log('');
-    alunos.forEach((aluno) => console.log(aluno.nome, aluno.email));
+    alunos.forEach((aluno) => console.log(aluno.nome.green, aluno.email.green));
     console.log('');
 
     // --->
 
     console.log('');
-    console.log("Buscando aluno pela Primary Key");
+    console.log("Buscando aluno pela Primary Key".red.underline);
 
     const aluno = await Aluno.findByPk(1);
     console.log('');
-    console.log(aluno.nome)
+    console.log(aluno.nome.green)
     console.log('');
 
     // --->
 
     console.log('');
-    console.log("Alterando dados do aluno")
+    console.log("Alterando dados do aluno".red.underline)
 
     const aluno1 = await Aluno.findByPk(3);
     aluno1.email = "emailteste@gmail.com";
     await aluno1.save();
     console.log('');
-    (await Aluno.findAll()).forEach((aluno) => console.log(aluno.nome, aluno.email));
+    (await Aluno.findAll()).forEach((aluno) => console.log(aluno.nome.green, aluno.email.green));
     console.log('');
 
     // --->
 
     console.log('');
-    console.log("Exclusão do Aluno");
+    console.log("Exclusão do Aluno".red.underline);
 
     const aluno2 = await Aluno.findByPk(3);
-    console.log(aluno2.nome);
+    console.log(aluno2.nome.green);
     await aluno2.destroy();
     console.log('');
-    (await Aluno.findAll()).forEach((aluno) => console.log(aluno.nome, aluno.email));
+    (await Aluno.findAll()).forEach((aluno) => console.log(aluno.nome.green, aluno.email.green));
     console.log('');
 
 })();
